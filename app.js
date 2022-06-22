c70cd0e40f83b5d1ce6d6af8c3e6676bf93966b9
Aditya Jadhav <jadhavaditya517@gmail.com>
2022-06-22 17:12:29 +0530

    Add .env

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const _ = require("lodash");
+require('dotenv').config();
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -11,7 +12,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://aditya:aditya@cluster0.cexhh.mongodb.net/todolistDB");
+const url = process.env.MONGO_URL;
+mongoose.connect(""+url);
 
 const itemsSchema = {
     name: String
